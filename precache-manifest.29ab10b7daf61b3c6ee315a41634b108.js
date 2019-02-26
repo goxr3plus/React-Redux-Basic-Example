eba5919db229456f603a490ac4a48dd79aeed1d9
Kentros Alexandros <goxr3plus@gmail.com>
2019-02-26 13:10:50 +0200

    Updates

---
+++ precache-manifest.29ab10b7daf61b3c6ee315a41634b108.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b7f65b1019aa39090ea",
+    "url": "/React-Redux-Basic-Example/static/css/main.c7cea92c.chunk.css"
+  },
+  {
+    "revision": "8b7f65b1019aa39090ea",
+    "url": "/React-Redux-Basic-Example/static/js/main.8b7f65b1.chunk.js"
+  },
+  {
+    "revision": "0fb4a57db85083cd8f9f",
+    "url": "/React-Redux-Basic-Example/static/js/runtime~main.0fb4a57d.js"
+  },
+  {
+    "revision": "50a8ee3822200aee1fbb",
+    "url": "/React-Redux-Basic-Example/static/js/2.50a8ee38.chunk.js"
+  },
+  {
+    "revision": "7bb257fa53030214d23ad748d5b29f55",
+    "url": "/React-Redux-Basic-Example/index.html"
+  }
+];
