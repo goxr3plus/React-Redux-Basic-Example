58915e43fd5a5f06db4573c135eae98e1269737c
Kentros Alexandros <goxr3plus@gmail.com>
2019-02-26 16:34:56 +0200

    Using Utility Function

--- src/store/reducers/counterReducer.js
+++ src/store/reducers/counterReducer.js
@@ -1,4 +1,5 @@
 import * as actionTypes from "../actions/actionTypes"
+import { updateObject } from "../utility"
 
 const initialState = {
    counter: 0,
@@ -7,26 +8,13 @@ const initialState = {
 const counterReducer = (state = initialState, action) => {
    switch (action.type) {
       case actionTypes.INCREMENT:
-         return {
-            ...state,
-            counter: state.counter + 1,
-         }
+         return updateObject(state, { counter: state.counter + 1 })
       case actionTypes.DECREMENT:
-         return {
-            ...state,
-            counter: state.counter - 1,
-         }
+         return updateObject(state, { counter: state.counter - 1 })
       case actionTypes.ADD:
-         return {
-            ...state,
-            counter: state.counter + action.value,
-         }
+         return updateObject(state, { counter: state.counter + action.value })
       case actionTypes.SUBSTRACT:
-         return {
-            ...state,
-            counter: state.counter / action.value,
-         }
-
+         return updateObject(state, { counter: state.counter / action.value })
       default:
          return state
    }
--- src/store/reducers/resultReducer.js
+++ src/store/reducers/resultReducer.js
@@ -1,4 +1,5 @@
 import * as actionTypes from "../actions/actionTypes"
+import { updateObject } from "../utility"
 
 const initialState = {
    results: [],
@@ -7,17 +8,11 @@ const initialState = {
 const resultReducer = (state = initialState, action) => {
    switch (action.type) {
       case actionTypes.STORE_RESULT:
-         return {
-            ...state,
+         return updateObject(state, {
             results: state.results.concat({ id: Math.random() * Math.random(), value: action.result }),
-         }
+         })
       case actionTypes.DELETE_RESULT:
-         const updatedArray = state.results.filter(result => result.id !== action.resultElementId)
-         return {
-            ...state,
-            results: updatedArray,
-         }
-
+         return updateObject(state, { results: state.results.filter(result => result.id !== action.resultElementId) })
       default:
          return state
    }
